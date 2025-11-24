8b23527df60f46360b7c4af0282d384e9aa123fe
JABNIA <vaxojabnia@gmail.com>
2025-11-24 19:54:56 +0400

    formatter fixed

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -5,7 +5,7 @@ sap.ui.define([], () => {
         formatDate(sDate) {
             const oBundle = this.getView().getModel("i18n").getResourceBundle();
 
-            if (sDate !== null) return `${oBundle.getText("published")}: ${sDate.substring(0, 4)}`;
+            if (sDate !== null) return oBundle.getText("published", [sDate.substring(0, 4)]);
         },
     };
 });
